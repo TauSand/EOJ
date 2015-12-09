15f5b3efe044de61478ace16c917ebb96848e8b3
dani <danirossi77@gmail.com>
2015-12-09 16:05:52 +0100

    notificationbar component

--- js/directives.js
+++ js/directives.js
@@ -1,25 +1,45 @@
 var directives = angular.module('directives', []);
 
-directives.directive('header', function() {
+directives.directive('header', function () {
     return {
         scope: {},
         restrict: 'E',
         templateUrl: 'templates/header.html',
-        link: function(scope, elem, attrs) {
+        link: function (scope, elem, attrs) {
             scope.open = "hello";
-            scope.openClick = function() {
+            scope.openClick = function () {
                 scope.open = !scope.open
             }
         }
     }
 });
 
-directives.directive('sidebar', function() {
+directives.directive('sidebar', function () {
     return {
         scope: {
             open: '@'
         },
         restrict: 'E',
         templateUrl: 'templates/sidebar.html'
+    }
+});
+
+directives.directive('notificationbar', function () {
+    return {
+        scope: {},
+        restrict: 'E',
+        templateUrl: 'templates/notificationbar.html'
+    }
+});
+
+
+directives.directive('barbutton', function () {
+    return {
+        scope: {
+            icon: '@',
+            notifications: '@'
+        },
+        restrict: 'E',
+        templateUrl: 'templates/barbutton.html'
     }
 });
