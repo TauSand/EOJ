831350bd9f3509cad1926ff7d43b0637c97a3fb4
dani <danirossi77@gmail.com>
2016-01-13 13:53:48 +0100

    update

--- sul-data.js
+++ sul-data.js
@@ -6,40 +6,40 @@ Sul = [
     {
         title: "Funktionsniveau",
         states: [
-            {"Problem relateret til personlig pleje": ""},
-            {"Problem relateret til daglige aktiviteter": ""},
+            {"Problem relateret til personlig pleje": {precision: "", comments: []}},
+            {"Problem relateret til daglige aktiviteter": {precision: "", comments: []}},
         ]
     },
 
     {
         title: "Bevægeapparat",
         states: [
-            {"Problem vedr. mobilitet og bevægelse": ""}
+            {"Problem vedr. mobilitet og bevægelse": {precision: "", comments: []}}
         ]
     },
 
     {
         title: "Ernæring",
         states: [
-            {"Problem vedr. væskeindtag": ""},
-            {"Problem vedr. fødeindtag": ""},
-            {"Vægtændring": ""},
-            {"Overvægt": ""},
-            {"Undervægt": ""}
+            {"Problem vedr. væskeindtag": {precision: "", comments: []}},
+            {"Problem vedr. fødeindtag": {precision: "", comments: []}},
+            {"Vægtændring": {precision: "", comments: []}},
+            {"Overvægt": {precision: "", comments: []}},
+            {"Undervægt": {precision: "", comments: []}}
         ]
     },
 
     {
         title: "Hud og slimhinder",
         states: [
-            {"Kirurgisk sår": ""},
-            {"Diabetisk sår": ""},
-            {"Cancersår": ""},
-            {"Tryksår": ""},
-            {"Arterielle sår": ""},
-            {"Hudproblem": ""},
-            {"Blandingssår": ""},
-            {"Traumesår": ""},
+            {"Kirurgisk sår": {precision: "", comments: []}},
+            {"Diabetisk sår": {precision: "", comments: []}},
+            {"Cancersår": {precision: "", comments: []}},
+            {"Tryksår": {precision: "", comments: []}},
+            {"Arterielle sår": {precision: "", comments: []}},
+            {"Hudproblem": {precision: "", comments: []}},
+            {"Blandingssår": {precision: "", comments: []}},
+            {"Traumesår": {precision: "", comments: []}},
 
         ]
     },
@@ -47,88 +47,88 @@ Sul = [
     {
         title: "Kommunikation",
         states: [
-            {"Kommunikationsvanskeligheder": ""}
+            {"Kommunikationsvanskeligheder": {precision: "", comments: []}}
         ]
     },
 
     {
         title: "Psykosociale forhold",
         states: [
-            {"Problem i samvær med andremennesker": ""},
-            {"Emotionelt problem": ""},
-            {"Kognitivt problem": ""},
-            {"Hukommelsessvækkelse": ""},
-            {"Misbrugsproblem": ""},
-            {"Impulskontrolforstyrrelse": ""},
-            {"Mentalt problem": ""}
+            {"Problem i samvær med andremennesker": {precision: "", comments: []}},
+            {"Emotionelt problem": {precision: "", comments: []}},
+            {"Kognitivt problem": {precision: "", comments: []}},
+            {"Hukommelsessvækkelse": {precision: "", comments: []}},
+            {"Misbrugsproblem": {precision: "", comments: []}},
+            {"Impulskontrolforstyrrelse": {precision: "", comments: []}},
+            {"Mentalt problem": {precision: "", comments: []}}
         ]
     },
 
     {
         title: "Respiration",
         states: [
-            {"Respirationsproblem": ""}
+            {"Respirationsproblem": {precision: "", comments: []}}
         ]
     },
     
     {
         title: "Cirkulation",
         states: [
-            {"Cirkulationsproblem": ""}
+            {"Cirkulationsproblem": {precision: "", comments: []}}
         ]
     },
 
     {
         title: "Seksualitet",
         states: [
-            {"Problem vedr.seksualitet/seksuel aktivitet": ""}
+            {"Problem vedr.seksualitet/seksuel aktivitet": {precision: "", comments: []}}
         ]
     },
 
     {
         title: "Smerte",
         states: [
-            {"Akutte smerter": ""},
-            {"Periodevise smerter": ""},
-            {"Kroniske smerter": ""}
+            {"Akutte smerter": {precision: "", comments: []}},
+            {"Periodevise smerter": {precision: "", comments: []}},
+            {"Kroniske smerter": {precision: "", comments: []}}
         ]
     },
 
     {
         title: "Sanseindtryk",
         states: [
-            {"Problem vedr. lugtesans": ""},
-            {"Problem vedr. følesans": ""},
-            {"Problem vedr. syn": ""},
-            {"Problem vedr. hørelse": ""},
-            {"Problem vedr. smagssans": ""}
+            {"Problem vedr. lugtesans": {precision: "", comments: []}},
+            {"Problem vedr. følesans": {precision: "", comments: []}},
+            {"Problem vedr. syn": {precision: "", comments: []}},
+            {"Problem vedr. hørelse": {precision: "", comments: []}},
+            {"Problem vedr. smagssans": {precision: "", comments: []}}
         ]
     },
 
     {
         title: "Søvn og hvile",
         states: [
-            {"Forstyrrelse af døgnrytme": ""},
-            {"Søvnforstyrrelse": ""}
+            {"Forstyrrelse af døgnrytme": {precision: "", comments: []}},
+            {"Søvnforstyrrelse": {precision: "", comments: []}}
         ]
     },
 
     {
         title: "Viden og udvikling",
         states: [
-            {"Utilstrækkelig indsigt i behandling": ""},
-            {"Utilstrækkelig sygdomsindsigt": ""}
+            {"Utilstrækkelig indsigt i behandling": {precision: "", comments: []}},
+            {"Utilstrækkelig sygdomsindsigt": {precision: "", comments: []}}
         ]
     },
 
     {
         title: "Udskillelse af affaldsstoffer",
         states: [
-            {"Afføringsinkontinens": ""},
-            {"Vandladningsproblem": ""},
-            {"Urininkontinens": ""},
-            {"Mave-tarmproblem": ""},
-            {"Problem med væske fra dræn": ""}
+            {"Afføringsinkontinens": {precision: "", comments: []}},
+            {"Vandladningsproblem": {precision: "", comments: []}},
+            {"Urininkontinens": {precision: "", comments: []}},
+            {"Mave-tarmproblem": {precision: "", comments: []}},
+            {"Problem med væske fra dræn": {precision: "", comments: []}}
 
         ]
     }
